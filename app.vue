88e94f2ecd3f12e2d8ed4fae754ccca329cb5f06
StephDietz <steph.dietz@vercel.com>
2023-06-07 08:29:18 -0500

    add styling

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="bg-zinc-800">
+	<div class="bg-gray-50">
 		<NuxtLayout>
 			<NuxtPage />
 		</NuxtLayout>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <script setup>
 	const messages = ref([]);
-	const answer = ref(null);
+	const response = ref(null);
 
-	const question = ref('');
+	const prompt = ref('');
 
-	const getAnswer = async ({ messages }) => {
+	const getResponse = async ({ messages }) => {
 		const { body } = await fetch('/api/chat', {
 			method: 'POST',
 			body: JSON.stringify({
@@ -15,93 +15,136 @@
 
 		return body;
 	};
-	const askQuestion = async () => {
+	const sendPrompt = async () => {
 		messages.value.push({
 			role: 'user',
-			content: question.value
+			content: prompt.value
 		});
-		question.value = '';
-		const stream = await getAnswer({ messages: messages.value });
-		answer.value = {
+		prompt.value = '';
+
+		const stream = await getResponse({ messages: messages.value });
+
+		response.value = {
 			role: 'assistant',
 			content: ''
 		};
 		useChatStream({
 			stream,
 			onChunk: ({ data }) => {
-				answer.value.content += data;
+				response.value.content += data;
 			},
 			onReady: () => {
-				messages.value.push(answer.value);
-				answer.value = null;
+				messages.value.push(response.value);
+				response.value = null;
 			}
 		});
 	};
 </script>
 
 <template>
-	<div class="mb-8 text-4xl font-bold text-center">AI Chatbot</div>
-	<div class="max-w-xl mx-auto">
-		<div class="bg-white rounded-md shadow h-[80vh] flex flex-col justify-between">
-			<ul class="overflow-auto">
-				<li v-for="message in messages" class="flex flex-col p-4">
-					<div v-if="message.role === 'assistant'" class="pr-8 mr-auto">
-						<div class="p-2 mt-1 text-sm text-gray-700 bg-gray-200 rounded-lg text-smp-2">
-							{{ message.content }}
+	<div class="max-w-xl mx-auto text-black">
+		<a
+			href="https://vercel.com/templates/next.js/blob-sveltekit"
+			class="flex justify-center px-10 py-2 mx-auto mt-20 space-x-1 text-sm font-medium text-center text-gray-600 transition-all rounded-full shadow-sm group sm:mt-0 bg-white/30 ring-1 ring-gray-900/5 hover:shadow-lg active:shadow-sm"
+		>
+			Deploy your own to Vercel
+		</a>
+		<h1 class="my-8 text-5xl font-bold text-center text-black">AI Chatbot</h1>
+		<div class="max-w-xl mx-auto">
+			<div class="bg-white rounded-md shadow h-[60vh] flex flex-col justify-between">
+				<ul class="overflow-auto">
+					<li v-for="(message, i) in messages" :key="i" class="flex flex-col p-4">
+						<div v-if="message.role === 'assistant'" class="pr-8 mr-auto">
+							<div class="p-2 mt-1 text-sm text-gray-700 bg-gray-200 rounded-lg text-smp-2">
+								{{ message.content }}
+							</div>
 						</div>
-					</div>
-					<div v-else class="pl-8 ml-auto">
-						<div class="p-2 mt-1 text-sm text-white bg-blue-400 rounded-lg">
-							{{ message.content }}
+						<div v-else class="pl-8 ml-auto">
+							<div class="p-2 mt-1 text-sm text-white bg-blue-400 rounded-lg">
+								{{ message.content }}
+							</div>
 						</div>
-					</div>
-				</li>
-				<!-- Incoming message -->
-				<li v-if="answer" class="flex p-4">
-					<div class="pr-8">
-						<div class="p-2 mt-1 text-sm text-gray-700 bg-gray-200 rounded-lg text-smp-2">
-							{{ answer.content }}
+					</li>
+					<!-- Incoming message -->
+					<li v-if="response" class="flex p-4">
+						<div class="pr-8">
+							<div class="p-2 mt-1 text-sm text-gray-700 bg-gray-200 rounded-lg text-smp-2">
+								{{ response.content }}
+							</div>
 						</div>
-					</div>
-				</li>
-			</ul>
-			<form @submit.prevent="askQuestion">
-				<div class="flex items-center w-full p-4">
-					<input
-						v-model="question"
-						type="text"
-						placeholder="Type here..."
-						class="w-full p-1 text-sm text-black bg-transparent bg-gray-100 border rounded-md shadow border-white/40 grow"
-					/>
-					<button
-						type="submit"
-						class="flex items-center justify-center flex-none w-10 h-10 ml-2 bg-green-500 rounded-full"
-					>
-						<svg
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
+					</li>
+				</ul>
+				<form @submit.prevent="sendPrompt">
+					<div class="flex items-center w-full p-4">
+						<input
+							v-model="prompt"
+							type="text"
+							placeholder="Type here..."
+							class="w-full p-1 text-sm text-black bg-transparent bg-gray-100 border rounded-md shadow border-white/40 grow"
+						/>
+						<button
+							:disabled="response && response.content"
+							type="submit"
+							class="flex items-center justify-center flex-none w-10 h-10 ml-2 bg-green-500 rounded-full"
 						>
-							<path
-								d="M22 2L11 13"
-								stroke="white"
-								stroke-width="1.5"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-							<path
-								d="M22 2L15 22L11 13L2 9L22 2Z"
-								stroke="white"
-								stroke-width="1.5"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-						</svg>
-					</button>
-				</div>
-			</form>
+							<svg
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M22 2L11 13"
+									stroke="white"
+									stroke-width="1.5"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+								<path
+									d="M22 2L15 22L11 13L2 9L22 2Z"
+									stroke="white"
+									stroke-width="1.5"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</svg>
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="flex flex-col justify-center w-full my-8">
+			<div class="flex items-center justify-center my-2">
+				<span>Built with</span>
+				<a
+					href="https://nuxt.com/docs"
+					class="flex items-center mx-1 font-medium underline transition-colors underline-offset-4 hover:text-black/70"
+				>
+					<p>gpt-4</p>
+				</a>
+				<span>and</span>
+				<a
+					href="https://nuxt.com/docs"
+					class="flex items-center font-medium underline transition-colors underline-offset-4 hover:text-black/70"
+				>
+					<img src="/nuxt.svg" class="h-6 mx-2" />
+					<p>Nuxt</p>
+				</a>
+				.
+			</div>
+			<div class="flex flex-col items-center justify-center">
+				<a href="https://vercel.com">
+					<img src="/vercel.svg" alt="Vercel Logo" class="h-4 my-2 text-white" />
+				</a>
+				<a
+					href="https://github.com/vercel/examples/tree/main/storage/kv-redis-nuxt"
+					class="flex items-center font-medium underline transition-colors underline-offset-4 hover:text-black/70"
+				>
+					<img src="/github.svg" alt="GitHub Logo" class="h-4" />
+					<p class="ml-1">Source</p>
+				</a>
+			</div>
 		</div>
 	</div>
 </template>
