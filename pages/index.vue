b25a8f0f6dc31027bf42a5ceab657aa11f30b195
StephDietz <steph.dietz@vercel.com>
2023-06-08 11:48:50 -0500

    simplify api call

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,34 @@
 <script setup>
-	const messages = ref([]);
-	const response = ref(null);
-	const loading = ref(false);
-	const prompt = ref('');
-	const responseChunks = ref([]);
-
-	const getResponse = async ({ messages }) => {
-		const { body } = await fetch('/api/chat', {
-			method: 'POST',
-			body: JSON.stringify({
-				messages
-			})
-		});
-		if (!body) throw new Error('Unknown error');
-
-		return body;
-	};
-	const sendPrompt = async () => {
-		messages.value.push({
-			role: 'user',
-			content: prompt.value
-		});
-		prompt.value = '';
-
-		const response = await fetch('/api/chat', {
-			method: 'POST',
-			body: JSON.stringify({
-				messages: messages.value
-			})
-		});
-
-		const { chunks } = await response.json();
-
-		for (let chunk of chunks) {
-			responseChunks.value.push(chunk);
+	const messages = ref([
+		{
+			actor: 'AI',
+			message: 'Hello! How can I help you?',
+			loading: false
 		}
+	]);
+	const loading = ref(false);
+	const message = ref('');
+
+	const sendPrompt = async () => {
+		if (message.value === '') return;
+		messages.value.push({
+			actor: 'Human',
+			message: message.value
+		});
+		message.value = '';
+		const res = await fetch(`/api/chat`, {
+			body: JSON.stringify(messages.value.slice(1)),
+			method: 'post'
+		});
+		if (res.status === 200) {
+			const response = await res.json();
+			messages.value.push({
+				actor: 'AI',
+				message: response?.message
+			});
+			message.value = '';
+		}
+		console.log(messages);
 	};
 </script>
 
@@ -51,31 +45,31 @@
 			<div class="bg-white rounded-md shadow h-[60vh] flex flex-col justify-between">
 				<ul class="overflow-auto">
 					<li v-for="(message, i) in messages" :key="i" class="flex flex-col p-4">
-						<div v-if="message.role === 'assistant'" class="pr-8 mr-auto">
+						<div v-if="message.actor === 'AI'" class="pr-8 mr-auto">
 							<div class="p-2 mt-1 text-sm text-gray-700 bg-gray-200 rounded-lg text-smp-2">
-								{{ message.content }}
+								{{ message.message }}
 							</div>
 						</div>
 						<div v-else class="pl-8 ml-auto">
 							<div class="p-2 mt-1 text-sm text-white bg-blue-400 rounded-lg">
-								{{ message.content }}
+								{{ message.message }}
 							</div>
 						</div>
 					</li>
 					<!-- Incoming message -->
-					<li v-if="response" class="flex p-4">
+					<!-- <li v-if="response" class="flex p-4">
 						<div class="pr-8">
 							<div class="p-2 mt-1 text-sm text-gray-700 bg-gray-200 rounded-lg text-smp-2">
 								{{ response.content }}
 							</div>
 						</div>
-					</li>
+					</li> -->
 					<div class="flex justify-start p-4" v-if="loading"><span class="loader"></span></div>
 				</ul>
 				<form @submit.prevent="sendPrompt">
 					<div class="flex items-center w-full p-4">
 						<input
-							v-model="prompt"
+							v-model="message"
 							type="text"
 							placeholder="Type here..."
 							class="w-full p-1 text-sm text-black bg-transparent bg-gray-100 border rounded-md shadow border-white/40 grow"
